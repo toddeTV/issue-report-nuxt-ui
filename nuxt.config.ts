26e29ad6bc6edfb5b7a05f46a0c9999fbc1fb69d
Thorsten Seyschab <business@todde.tv>
2024-05-06 10:45:14 +0200

    feat: add code that will break the `pnpm build` command

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  modules: [
+    '@nuxt/ui',
+    '@tresjs/nuxt',
+  ],
+  ui: {
+    global: true, // this will break on `pnpm build` -> if set to `false` | `undefined` the build will work
+  },
 })
